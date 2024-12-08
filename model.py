081fe624293a88e99ca86a058db9493f2ea5145b
Arturo Lara <a.lara.2024@alumnos.urjc.es>
2024-12-08 16:17:21 +0100

    Añadidos comentarios en el código

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 from imblearn.over_sampling import SMOTE
 from sklearn.model_selection import train_test_split
-from sklearn.neighbors import NearestNeighbors, NeighborhoodComponentsAnalysis, KNeighborsClassifier
-from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.neighbors import NeighborhoodComponentsAnalysis, KNeighborsClassifier
+from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.feature_selection import VarianceThreshold
 from sklearn.metrics import confusion_matrix
@@ -23,7 +23,7 @@ class Model:
         input_preprocess = []
 
 
-        # eliminamos los datos que no varían casi (no dan mucha información)
+        # escalado de datos y eliminamos los datos que no varían casi (no dan mucha información)
         unit_scaler = StandardScaler().set_output(transform="pandas")
         input_preprocess.append(('scaler1', unit_scaler))
         feat_selector = VarianceThreshold(self.__var_th).set_output(transform='pandas')
@@ -31,17 +31,17 @@ class Model:
 
         preprocess_pipe = Pipeline(input_preprocess)
 
-        svm_clf  = SVC(kernel='poly', degree=12,probability=True)
-        rf_clf = RandomForestClassifier(n_estimators=1000)
+        svm_clf  = SVC(kernel='poly', degree=12,probability=True) #Modelo SVN
+        rf_clf = RandomForestClassifier(n_estimators=1000) #Modelo Random Forest
         clf_net = MLPClassifier(solver='adam', alpha=1e-4,
-                            hidden_layer_sizes=(30,40), random_state=1, max_iter=400, activation='tanh')
+                            hidden_layer_sizes=(30,40), random_state=1, max_iter=400, activation='tanh') #Modelo red MLP
         nca = NeighborhoodComponentsAnalysis(random_state=42)
         knn = KNeighborsClassifier(n_neighbors=3)
-        nca_knn_pipe = Pipeline([('nca', nca), ('knn', knn)])
+        nca_knn_pipe = Pipeline([('nca', nca), ('knn', knn)]) #Pipeline con el modelo por vecindad
 
         voting_clf = VotingClassifier(
             estimators=[('svm_clf', svm_clf), ('rf_clf', rf_clf), ('nca_knn', nca_knn_pipe), ('clf_net', clf_net)],
-            voting='soft')
+            voting='soft') #se define el sistema de votación y se incluyen los modelos
 
         classifier_pipe = Pipeline([('voting_clf', voting_clf)])
         self.__pipe = Pipeline([('preprocess', preprocess_pipe), ('classifier', classifier_pipe)])
@@ -52,7 +52,9 @@ class Model:
     def fit(self, x_dataset, y_dataset, output):
 
         sm = SMOTE(random_state=42)
-        x_res, y_res = sm.fit_resample(x_dataset, y_dataset)
+        x_res, y_res = sm.fit_resample(x_dataset, y_dataset) #creación de muestras sinteticas
+
+        #Split del dataset para obtener los subconjuntos de entrenamiento y test
         x_train_init, self.x_test, y_train_init, self.y_test = train_test_split(x_res, y_res, test_size=0.01, random_state=1234)
 
         self.__pipe.fit(x_train_init, y_train_init[['Crop']].values.ravel())
